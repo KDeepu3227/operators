84e9e28ea6b06f2b496e6311665c4040c9bdd49e
KDeepu3227 <deepugmd0317@gmail.com>
2024-09-23 18:56:07 +0530

    Create CCoachingcamp.py

---
+++ CCoachingcamp.py
@@ -0,0 +1,13 @@
+'''
+Dhoni once wished to join a a reputed Cricket Coaching Camp to be held at a place "X" kms away from his house. He told about this to his father and got his consent to use his friend's bike for the Camp. The Camp was to be held on all days of the month. His friend's bike provides a mileage of Y km/litre and the cost of petrol was Rs. Z. Dhoni's father now wanted to know the total amount that was needed by Dhoni to spend on his travel to the Camp. Help him find the same and assume number of days in a month as 30 days.
+'''
+x=float(input())
+y=float(input())
+z=float(input())
+a=(x*z*30)/y
+print("%.2f"%a)
+'''
+75
+55
+63
+2577.27
